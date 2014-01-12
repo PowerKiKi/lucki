62d1e54aa6b3b5078adeafce8444b0a891c1c166
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2014-01-12 13:05:35 +0900

    Fix carousel initialization

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,30 +1,32 @@
 (function() {
-  $(function() {
-    $("[href^=#none]").click(function(e) {
-      return e.preventDefault();
+    $(function() {
+        $("[href^=#none]").click(function(e) {
+            return e.preventDefault();
+        });
+
+        $("a[data-toggle=popover]").popover().click(function(e) {
+            return e.preventDefault();
+        });
+
+        $("a[data-toggle=tooltip]").tooltip();
+        $(".carousel").carousel({
+            interval: 5000,
+            pause: "hover"
+        });
+
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 100) {
+                return $(".scrollup").fadeIn();
+            } else {
+                return $(".scrollup").fadeOut();
+            }
+        });
+
+        $(".scrollup").click(function() {
+            return $("html, body").animate({
+                scrollTop: 0
+            }, 600, false);
+        });
     });
-    $("a[data-toggle=popover]").popover().click(function(e) {
-      return e.preventDefault();
-    });
-    $("a[data-toggle=tooltip]").tooltip();
-    $(function() {});
-    $(".carousel").carousel;
-    ({
-      interval: 5000,
-      pause: "hover"
-    });
-    $(window).scroll(function() {
-      if ($(this).scrollTop() > 100) {
-        return $(".scrollup").fadeIn();
-      } else {
-        return $(".scrollup").fadeOut();
-      }
-    });
-    $(".scrollup").click(function() {
-      return $("html, body").animate({
-        scrollTop: 0
-      }, 600, false);
-    });
-  });
 
 }).call(this);
