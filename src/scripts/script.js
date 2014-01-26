db73446c7891369687d7442bcc7eac0848b288ae
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2014-01-26 19:40:31 +0900

    Search/Tag/Category page first version

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -37,6 +37,32 @@
             time.parents('article').addClass('month-' + month);
             time.html(year + '<br>' + month + '월');
         });
-    });
 
+        // Create rows with X items per row
+        $('[data-make-rows]').each(function() {
+            var div = $(this);
+            var itemsPerRow = div.attr('data-make-rows');
+            var children = div.children();
+            var a;
+            var i = 0;
+            while ((a = children.slice(i, i += itemsPerRow)).length) {
+                var row = $('<div class="row">');
+                row.append(a);
+                div.append(row);
+            }
+        });
+
+        // Load article image preview, this require Facebook plugin to be active in Tistory plugins settings
+        $('[data-img-src]').each(function() {
+            var img = $(this);
+            var url = $(this).attr('data-img-src');
+
+            $.get(url, function(data) {
+                var imgUrl = $(data).filter('meta[property="og:image"]').attr('content');
+                if (imgUrl) {
+                    img.replaceWith($('<img class="img-responsive">').attr('src', imgUrl));
+                }
+            });
+        });
+    });
 }).call(this);
